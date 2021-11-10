b5cbdbf3d2fccad11f7df1587138f97944aa3e86
Jordan Kelly <63018389+profjjk@users.noreply.github.com>
2021-11-09 17:41:12 -0800

    add initial content to homepage

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    // Navigation links
+    $('#brand').on('click', e => {
+        e.preventDefault();
+        window.location.pathname = ('/');
+    });
+    $('.nav-link').on('click', e => {
+        e.preventDefault();
+        const page = e.target.innerHTML.toLowerCase();
+        window.location.pathname = (`/${page}`);
+    });
+    $('.btn-equipment').on('click', e => {
+        e.preventDefault();
+        window.location.pathname = ('/equipment');
+    })
+})
