98c94a01c29bb833b3f9f3a390fdb02e745fd0ed
Jordan Kelly <63018389+profjjk@users.noreply.github.com>
2022-01-04 16:52:44 -0800

    add dotenv package

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const cors = require('cors');
-require('dotenv/config');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5050;
