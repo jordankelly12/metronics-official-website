b43f510c8aed7f5fa798c41923769371b05f4084
Jordan Kelly <63018389+profjjk@users.noreply.github.com>
2022-01-14 16:49:05 -0800

    change api path

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,14 +2,14 @@ const db = require("../models");
 const express = require('express');
 const router = express.Router();
 
-router.post("/api/request", async (req, res) => {
+router.post("https://metronics-webapp-server.herokuapp.com/api/request", async (req, res) => {
     try {
         const data = await db.Request.create(req.body);
         res.json(data);
     } catch(err) { res.status(422).json({ msg: err}) }
 });
 
-router.post("/api/message", async (req, res) => {
+router.post("https://metronics-webapp-server.herokuapp.com/api/message", async (req, res) => {
     try {
         const data = await db.Message.create(req.body);
         res.json(data);
